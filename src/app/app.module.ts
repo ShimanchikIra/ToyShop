2956edae32971cf015ef2f2cc739dfd90269ed03
ShimanchikIra <shimanchikirina@gmail.com>
2020-01-13 23:51:50 +0300

    Add Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
+import { AboutShopComponent } from './main/about-shop/about-shop.component';
+import { CatalogComponent } from './main/catalog/catalog.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AboutShopComponent},
+  { path: 'catalog', component: CatalogComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    AboutShopComponent,
+    CatalogComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule,
-    BrowserAnimationsModule
+    // BrowserModule,
+    // RouterModule,
+    // BrowserAnimationsModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

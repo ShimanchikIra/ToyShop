fb2da4e24bd00c071468314635c5af71af3cb187
ShimanchikIra <shimanchikirina@gmail.com>
2020-01-15 20:52:49 +0300

    Fix styles in header and footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Config} from '../../config';
+import {ConfigService} from '../config.service';
+import {LogService} from '../log.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  config: Config;
+  status: number;
+  headers: string[];
 
-  constructor() { }
+  constructor(private configService: ConfigService) { }
 
   ngOnInit() {
+    this.getConfig();
+    LogService.write('Config has been got', this.status);
+  }
+  getConfig() {
+    this.configService.getConfig().subscribe(resp => {
+      const keys = resp.headers.keys();
+      this.headers = keys.map(key =>
+        `${key}: ${resp.headers.get(key)}`);
+      this.config = { ... resp.body };
+    });
   }
 
 }
